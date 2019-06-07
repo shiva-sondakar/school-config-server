3efbca33ea98ffb8f08a081d2c328ccf0a59b218
SHIVANAND SONDAKAR <shiva@SHIVANANDs-MacBook-Air.local>
2019-06-08 01:02:15 +0530

    renamed packages and classes

--- src/main/java/com/shiva/schoolconfigserver/SchoolConfigServerApplication.java
+++ src/main/java/com/shiva/schoolconfigserver/SchoolConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.shiva.shoppingcartconfigserver;
+package com.shiva.schoolconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ShoppingCartConfigServerApplication {
+public class SchoolConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingCartConfigServerApplication.class, args);
+		SpringApplication.run(SchoolConfigServerApplication.class, args);
 	}
 
 }
